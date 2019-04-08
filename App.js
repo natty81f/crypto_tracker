93cc4528f9c3c136c27ebcde3d640dc4c11b9016
Fierro, Natalia <FierroN@dnb.com>
2019-04-08 14:43:07 -0700

    initial commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { View } from "react-native";
+import { Header } from "./src/components";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <Header />
+      </View>
+    );
+  }
+}
